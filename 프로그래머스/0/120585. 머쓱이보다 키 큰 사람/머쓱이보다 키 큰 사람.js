8bfaf802fa4b53cc86e3b1720d6ee1fd8e37b49f
김수연 <143993211+kimsudang@users.noreply.github.com>
2024-01-30 12:45:42 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,11 +1,4 @@
 function solution(array, height) {
-    let answer = 0;
-    array = array.sort((a,b) => a - b);
-    console.log(array);
-    for(let i = 0; i < array.length; i++){
-        if(array[i] > height){
-            answer++;
-        }
-    }
-    return answer;
+    const answer = array.filter((tall) => tall > height);
+    return answer.length;
 }
