5d852c90e01c3be9f37150ba2471bc10e433586e
김수연 <143993211+kimsudang@users.noreply.github.com>
2024-02-08 12:47:24 +0900

    [Silver IV] Title: 수 찾기, Time: 344 ms, Memory: 41252 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const arr1 = input[1].split(' ').map(Number);
-const arr2 = input[3].split(' ').map(Number);
+const arr1 = input[1].split(' ').map(Number); // arr1로 입력받는 부분 변경
+const arr2 = input[3].split(' ').map(Number); // arr2로 입력받는 부분 변경
 
-arr1.sort((a, b) => a - b);
+arr1.sort((a, b) => a - b); // arr1을 정렬
 
 const result = [];
 
 for (let i = 0; i < arr2.length; i++) {
   let found = false;
+
+  // 정렬된 배열 arr1에서 arr2[i]를 이진 탐색하여 찾음
   let left = 0;
   let right = arr1.length - 1;
   while (left <= right) {
@@ -23,7 +25,7 @@ for (let i = 0; i < arr2.length; i++) {
       right = mid - 1;
     }
   }
-  
+
   if (found) {
     result.push(1);
   } else {
@@ -31,4 +33,4 @@ for (let i = 0; i < arr2.length; i++) {
   }
 }
 
-console.log(result.join(' '));
+console.log(result.join(' ')); // 결과를 공백으로 구분하여 출력
