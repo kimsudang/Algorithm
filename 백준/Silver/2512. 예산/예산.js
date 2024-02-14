6fa844a6e7f0935118229e08301c9f2368e313a3
김수연 <143993211+kimsudang@users.noreply.github.com>
2024-02-14 14:35:35 +0900

    [Silver II] Title: 예산, Time: 220 ms, Memory: 12196 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.js
@@ -0,0 +1,41 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+rl.on('line', function(line) {
+    input.push(line);
+}).on('close', function() {
+    const N = parseInt(input[0]);
+    const budgets = input[1].split(' ').map(Number);
+    const M = parseInt(input[2]);
+
+    function solution(budgets, M) {
+        budgets.sort((a, b) => a - b); // 오름차순 정렬
+        let left = 0;
+        let right = budgets[budgets.length - 1]; // 최대 상한선은 가장 큰 예산으로 설정
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            let total = 0;
+
+            for (let budget of budgets) {
+                total += (budget > mid) ? mid : budget;
+            }
+
+            if (total > M) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right; // 최대 상한선을 반환
+    }
+
+    console.log(solution(budgets, M));
+    process.exit();
+});
