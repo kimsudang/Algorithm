2b005f17b62a50ab04fcfd66f8b341a23cdeac2b
김수연 <143993211+kimsudang@users.noreply.github.com>
2024-02-16 04:02:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 192 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Silver III/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = parseInt(input[0]);
+
+let dp = new Array(n + 1).fill(0);
+
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[n] + '\n');
+
